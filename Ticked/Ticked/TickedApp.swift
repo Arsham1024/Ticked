4aea4cc533b5e579c0592a44320fd9ebd10343fb
Arsham1024 <Arsham512@gmail.com>
2023-08-19 18:01:35 -0700

    create the project

---
+++ Ticked/Ticked/TickedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickedApp.swift
+//  Ticked
+//
+//  Created by Arsham Mehrani on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TickedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
